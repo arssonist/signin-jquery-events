23b4488824844315c0094c40bda695835721e5e2
Your Name <arssonist@yahoo.com>
2016-09-13 17:18:23 -0400

    error messages working

--- js/main.js
+++ js/main.js
@@ -7,3 +7,21 @@ $('.signin').click(function() {
 $('.close').click(function() {
    $('.modal').fadeOut('slow')
  });
+$('.submit').click(function() {
+  $("input[name='user']").attr("placeholder", "Error");
+})
+$('.submit').click(function() {
+  $("input[name='pass']").attr("placeholder", "Error");
+})
+
+
+
+// ("input[type='checkbox'][name='ProductCode']")
+//
+// // $("input:checkbox[name='ProductCode']")
+//
+// $('#name').attr('placeholder', 'identify yourself');
+//
+// - $('#email').attr('value', 'koalathebear@gmail.com');
+//
+// $( "input[value='Hot Fuzz']" )
